fd92061933dd5f7d8d90e6eb4238472ee6b56dbb
Anthony <anttyaadiggg@yahoo.com>
2016-07-21 17:29:17 -0400

    added assets

---
+++ config/initializers/assets.rb
@@ -0,0 +1,4 @@
+Rails.application.config.assets.precompile += %w( grayscale.min.css grayscale.min.js)
+Rails.application.config.assets.precompile += %w( grayscale.css grayscale.js)
+Rails.application.config.assets.precompile += %w( agency.css agency.min.css )
+Rails.application.config.assets.precompile += %w( agency.js contact_me.js agency.min.js jqBootstrapValidation.js )
